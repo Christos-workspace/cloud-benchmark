0c435afb70703d38785242cfe611d22d6255f0f3
Data-Framed <dataframedhq@gmail.com>
2025-07-22 22:32:19 +0200

    update scraper, main

--- container/main.py
+++ container/main.py
@@ -8,7 +8,6 @@ REUTERS_URL = "https://www.reuters.com/"
 
 
 def main():
-    # Example configuration for BBC
     bbc_config = SiteConfig(
         base_url=BBC_URL,
         news_url=f"{BBC_URL}/news",
@@ -16,24 +15,36 @@ def main():
         card_selector='div[data-testid="anchor-inner-wrapper"]',
         title_selector="h2",
         link_selector="a",
+        keyword='article',
         summary_selector="p[data-testid='card-description']",
         date_selector="time",
+        date_attribute="datetime"
     )
 
+    # apnews_config = SiteConfig(
+    #     base_url=APNEWS_URL,
+    #     section_selector="div.FourColumnContainer-column",
+    #     card_selector="div.PagePromo-content",
+    #     title_selector="span.PagePromoContentIcons-text",
+    #     link_selector="a",
+    #     keyword='article',
+    #     date_selector="bsp-timestamp",
+    #     date_atttribute="data-timestamp"
+    # )
+    # #
     # Create scrapers for all sites
-    all_results = NewsScraper.scrape_all_sites([
+    all_articles = NewsScraper.scrape_all_sites([
         bbc_config,
+        # apnews_config,
+
+
         # Add similar configs for APNEWS and REUTERS here
     ])
 
     # Save results
-    with open('articles.json', 'w') as f:
+    with open('container/articles.json', 'w') as f:
         json.dump(
-            {
-                site_name: [article.model_dump(mode='json')
-                            for article in articles_list]
-                for site_name, articles_list in all_results.items()
-            },
+            [article.model_dump(mode='json') for article in all_articles],
             f,
             ensure_ascii=False,
             indent=4)
--- container/scraper.py
+++ container/scraper.py
@@ -1,10 +1,11 @@
-
+import time
 import requests
 from bs4 import BeautifulSoup as bs
 from pydantic import BaseModel, Field, HttpUrl
 from typing import Dict, List, Optional
 from fake_useragent import UserAgent
-from datetime import datetime as dt, date
+from datetime import datetime as dt, timezone, date
+from dateutil import parser
 from loguru import logger
 
 # Log to file and rotate every 10 MB
@@ -12,6 +13,7 @@ logger.add("container/scraper.log", rotation="10 MB")
 
 
 class NewsArticle(BaseModel):
+    site_name: str
     title: str
     url: HttpUrl
     published_date: date
@@ -26,8 +28,10 @@ class SiteConfig(BaseModel):
     card_selector: str
     title_selector: str
     link_selector: str
-    summary_selector: str
+    keyword: Optional[str] = None
+    summary_selector: Optional[str] = None
     date_selector: str
+    date_attribute: Optional[str] = None
 
 
 class NewsScraper:
@@ -52,6 +56,7 @@ class NewsScraper:
         logger.info(f"Fetching URL: {url}")
         response = requests.get(
             url, headers={"User-Agent": UserAgent().random})
+        time.sleep(4)
         if response.status_code != 200:
             logger.error(f"Failed to fetch data from {
                          url} - Status code: {response.status_code}")
@@ -63,18 +68,29 @@ class NewsScraper:
     @staticmethod
     def parse_date(date_str: str) -> date:
         """
-        Parese a date string into a date object.
+        Parse a date string into a date object.
         """
         try:
-            return dt.fromisoformat(date_str.replace('Z', '+00:00')).date()
+            if date_str.isdigit():
+                return dt.fromtimestamp(int(date_str)/1000, tz=timezone.utc).date()
+            else:
+                return parser.parse(date_str).date()
+
         except ValueError:
+            logger.error(f"Error parsing date string {
+                         date_str}, applying default date")
             return date.today()
 
     def scrape_site(self) -> List[NewsArticle]:
         """Scrape articles from the configured news site"""
         logger.info(f"Starting scraping for site: {self.config.base_url}")
+        if not self.config.news_url:
+            logger.warning("No newa URL provided, using base URL instead.")
+            self.config.news_url = self.config.base_url
         soup = self.get_soup(str(self.config.news_url))
+        site_name = self.config.base_url.host
         sections = soup.select(self.config.section_selector)
+        logger.debug(f"Found {len(sections)} sections in the page")
 
         articles = []
         unique_links = set()
@@ -99,12 +115,14 @@ class NewsScraper:
                 if not link.startswith("http"):
                     link = f"{self.config.base_url}{link}"
 
+                logger.debug(f"Processing link: {link}")
+
                 if link in unique_links:
                     logger.debug(f"Duplicate link found: {link}, skipping...")
                     continue
 
-                if 'articles' not in link:
-                    logger.debug(f"Link does not contain 'articles': {
+                if 'article' not in link:
+                    logger.debug(f"Link does not contain article: {
                                  link}, skipping...")
                     continue
 
@@ -117,16 +135,32 @@ class NewsScraper:
                         "No title found for article card, skipping...")
                     continue
                 title = title_elem.get_text().strip()
+                logger.debug(f"Article title: {title}")
 
-                summary_elem = card.select_one(self.config.summary_selector)
-                summary = summary_elem.get_text().strip() if summary_elem else ""
+                if self.config.summary_selector:
 
-                # Get publication date
+                    summary_elem = card.select_one(
+                        self.config.summary_selector)
+                    summary = summary_elem.get_text().strip() if summary_elem else ""
+                else:
+                    summary = ""
+
                 date_elem = card.select_one(self.config.date_selector)
-                date_str = date_elem.get('datetime') if date_elem else ""
+                if not date_elem:
+                    logger.warning(f"No date found for article card: {
+                                   title}, trying to fetch from link...")
+                    time_soup = self.get_soup(link)
+                    date_elem = time_soup.select_one(self.config.date_selector)
+
+                date_str = date_elem.get(
+                    self.config.date_attribute)
+                logger.debug(f"Date string found for {
+                             self.config.date_attribute}: {date_str}")
+
                 pub_date = self.parse_date(date_str)
 
                 articles.append(NewsArticle(
+                    site_name=site_name,
                     title=title,
                     url=link,
                     published_date=pub_date,
@@ -140,23 +174,20 @@ class NewsScraper:
         return articles
 
     @staticmethod
-    def scrape_all_sites(configs: List[SiteConfig]) -> Dict[str, List[NewsArticle]]:
+    def scrape_all_sites(configs: List[SiteConfig]) -> List[NewsArticle]:
         """Scrape multiple sites and return results grouped by site"""
-        results = {}
+        all_articles = []
         for config in configs:
             logger.info(f"Scraping site: {config.base_url}")
             scraper = NewsScraper(config)
-            site_name = config.base_url.host  # type: ignore
             try:
-                results[site_name] = scraper.scrape_site()
+                articles = scraper.scrape_site()
+                all_articles.extend(articles)
             except Exception as e:
-                logger.critical(f"Scraping failed for {site_name}: {e}")
-                results[site_name] = []
-        results_summary = {
-            site: len(articles) for site, articles in results.items()
-        }
+                logger.critical(f"Scraping failed for {
+                                config.base_url.host}: {e}")
+                articles = []
         logger.info(
-            f"All sites scraped. "
-            f"Results: {results_summary}"
+            f"All sites scraped. Total articles found: {len(all_articles)}"
         )
-        return results
+        return all_articles
